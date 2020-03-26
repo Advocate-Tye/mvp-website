b680d23d8ac839f1ca3b655a34e20de87c28f2da
Steven He <therealonestig@gmail.com>
2020-03-26 14:49:49 -0700

    Re routing pages pre beta test

--- routing/main.js
+++ routing/main.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated } = require('../config/authenticate');
+const User = require('../models/user');
 
 //router.get('/', (req, res) => res.render('welcome'));
 
  router.get('/', (req, res) => {
-   res.render('comingsoon');
+   res.render('landing');
    //console.log(req)
  });
 
 //router.get('/', (req, res) => res.redirect('/users/login'));
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
-  if (req.user.isbusiness == false) {
-    res.render('dashboard', {user: req.user});
-  } else {
-    res.render('businessdashboard', {user: req.user});
-  }
+  User.countDocuments({}, function (err, count) {
+    res.render('dashboard', {user: req.user, count})
+  });
+
+  // if (req.user.isbusiness == false) {
+  //   res.render('dashboard', {user: req.user});
+  // } else {
+  //   res.render('businessdashboard', {user: req.user});
+  // }
 
   //console.log(req);
 });
